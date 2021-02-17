d4e21a69638cd408235c2564f4dc50718238963b
Yubeli30 <69699685+Yubeli30@users.noreply.github.com>
2021-02-17 09:44:27 -0500

    updata

--- business-intelligence-web/src/App.vue
+++ business-intelligence-web/src/App.vue
@@ -6,31 +6,31 @@
     </a>
     <div class="flex-grow flex flex-col justify-between text-gray-500 bg-gray-800">
       <nav class="flex flex-col mx-4 my-6 space-y-4">
-        <router-link to="/" class="inline-flex items-center justify-center py-3 hover:bg-white hover:text-purple-900 focus:text-gray-400 focus:bg-gray-700 rounded-lg">
+        <router-link to="/" class="inline-flex items-center justify-center py-3 hover:text-gray-400 hover:bg-gray-700 focus:text-gray-400 focus:bg-gray-700 rounded-lg">
           <span class="sr-only">Inicio</span>
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
           </svg>
         </router-link>
-        <router-link to="/about" class="inline-flex items-center justify-center py-3 hover:bg-white hover:text-purple-900 focus:text-gray-400 focus:bg-gray-700 rounded-lg">
+        <router-link to="/about" class="inline-flex items-center justify-center py-3 text-gray-600 hover:bg-white hover:text-purple-900 focus:text-gray-400 focus:bg-gray-700 rounded-lg">
           <span class="sr-only">Dashboard</span>
           <svg aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" />
           </svg>
         </router-link>
-        <router-link to="/hola" class="inline-flex items-center justify-center py-3 hover:bg-white hover:text-purple-900 focus:text-gray-400 focus:bg-gray-700 rounded-lg">
+        <router-link to="/hola" class="inline-flex items-center justify-center py-3 hover:text-gray-400 hover:bg-gray-700 focus:text-gray-400 focus:bg-gray-700 rounded-lg">
           <span class="sr-only">Folders</span>
           <svg aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 7v10a2 2 0 002 2h14a2 2 0 002-2V9a2 2 0 00-2-2h-6l-2-2H5a2 2 0 00-2 2z" />
           </svg>
         </router-link>
-       <router-link to="/dashboard"  class="inline-flex items-center justify-center py-3 hover:bg-white hover:text-purple-900 focus:text-gray-400 focus:bg-gray-700 rounded-lg">
+       <router-link to="/dashboard"  class="inline-flex items-center justify-center py-3 hover:text-gray-400 hover:bg-gray-700 focus:text-gray-400 focus:bg-gray-700 rounded-lg">
           <span class="sr-only">Messages</span>
           <svg aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
           </svg>
         </router-link>
-        <router-link to="/ver" class="inline-flex items-center justify-center py-3 hover:bg-white hover:text-purple-900 focus:text-gray-400 focus:bg-gray-700 rounded-lg">
+        <router-link to="/ver" class="inline-flex items-center justify-center py-3 hover:text-gray-400 hover:bg-gray-700 focus:text-gray-400 focus:bg-gray-700 rounded-lg">
           <span class="sr-only">Documents</span>
           <svg aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 21h10a2 2 0 002-2V9.414a1 1 0 00-.293-.707l-5.414-5.414A1 1 0 0012.586 3H7a2 2 0 00-2 2v14a2 2 0 002 2z" />
