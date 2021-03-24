adef4b865ea79472141e0b7c03a74d4d96c97c22
Yubeli30 <syrivera89@misena.edu.co>
2021-03-24 15:09:08 -0500

    add

---
+++ business-intelligence-web/src/components/Maps/MapsSilver.js
@@ -0,0 +1,43 @@
+// import { MapElementFactory } from "vue3-google-map ";
+
+// export default MapElementFactory({
+//   name: "directionsRenderer",
+
+//   ctr() {
+//     return window.google.maps.DirectionsRenderer;
+//   },
+
+//   events: [],
+
+//   mappedProps: {},
+
+//   props: {
+//     origin: { type: Object },
+//     destination: { type: Object },
+//     travelMode: { type: String }
+//   },
+
+//   afterCreate(directionsRenderer) {
+//     let directionsService = new window.google.maps.DirectionsService();
+
+//     this.$watch(
+//       () => [this.origin, this.destination, this.travelMode],
+//       () => {
+//         let { origin, destination, travelMode } = this;
+//         if (!origin || !destination || !travelMode) return;
+
+//         directionsService.route(
+//           {
+//             origin,
+//             destination,
+//             travelMode
+//           },
+//           (response, status) => {
+//             if (status !== "OK") return;
+//             directionsRenderer.setDirections(response);
+//           }
+//         );
+//       }
+//     );
+//   }
+// });
--- business-intelligence-web/src/views/Maps/MapsCreate.vue
+++ business-intelligence-web/src/views/Maps/MapsCreate.vue
@@ -1,7 +1,17 @@
 <template>
 
-<div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-3">
-    <p>Crear mapa</p>
+<div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-3 ">
+    <GoogleMap
+      api-key="AIzaSyDiuJUZXx0ozvPqOcpkW7AduGh4Rvp0Flo"
+      style="width: 100%; height: 500px"
+      :center="LaPlata"
+      :zoom="15" >
+    <Marker :options="{ position: LaPlata, icon: Marcador, size: 20 }" @click="hola()" />
+    <Marker :options="{ position: Nataga, icon: Marcador, size: 10 }" @click="hola()"/>
+  </GoogleMap>
+
+
+
 
 </div>
 
@@ -11,19 +21,36 @@
 
 <script>
 
+import { ref } from 'vue'
 import API_ROUTER from './../../services/SERVER_API'
-
+import { GoogleMap, Marker } from 'vue3-google-map'
+// import DirectionsRenderer from './../../components/Maps/MapsSilver.js'
 
 export default {
-   data(){
+    components: { 
+      GoogleMap, Marker
+    },
+    data(){
+
       return{
         name: 'MapsCreate',
-        Img1: API_ROUTER.API_UI+'maps/1.png',
-        Img2: API_ROUTER.API_UI+'maps/2.png',
-        Img3: API_ROUTER.API_UI+'maps/3.png',
-
+        Marcador: API_ROUTER.API_UI+'maps/location.png',
+        LaPlata: { lat: 2.3877584, lng: -75.8934048 },
+        Nataga: { lat: 2.545506, lng: -75.8096025 }, 
+        start: "",
+        end: ""   
       }
     },
+    mounted(){
+
+
+    },
+    methods:{
+      hola(){
+        console.log('hola')
+      }
+
+    }
   
 }
 </script>
