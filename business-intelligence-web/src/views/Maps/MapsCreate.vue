bd47a7c1fea2426dc794afe341bf7b410dedfe38
Yubeli30 <syrivera89@misena.edu.co>
2021-03-24 23:22:52 -0500

    Update

--- business-intelligence-web/src/views/Maps/MapsCreate.vue
+++ business-intelligence-web/src/views/Maps/MapsCreate.vue
@@ -1,88 +1,87 @@
 <template>
-
-<div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-3 ">
+  <div class="px-4 py-16 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 lg:py-3 ">
     <GoogleMap
       api-key="AIzaSyDiuJUZXx0ozvPqOcpkW7AduGh4Rvp0Flo"
-      style="width: 100%; height: 700px"
+      style="width: 100%; height: 800px"
+      ref="map" 
+      id="map"
       :center="LaPlata"
-      :zoom="5" >
-      <div travelMode="driving" :origin="origin" :destination="destionation" />
-    <Marker :options="{ position: LaPlata, icon: Marcador, size: 20 }" @click="hola()" />
-    <Marker :options="{ position: Nataga, icon: Marcador, size: 10 }" @click="hola()"/>
-  </GoogleMap>
+      :zoom="18" >
+        <Marker :options="{ position: LaPlata, icon: Marcador, size: 20 }" @click="hola()" />
+        <Marker :options="{ position: Nataga, icon: Marcador, size: 10 }" @click="hola()"/>
+        <Marker :options="{ position: LaPlata2, icon: Marcador, size: 10 }" @click="hola()"/>
 
-  <div>
-    <select v-model="start">
-      <option value="chicago, il">Chicago</option>
-      <option value="st louis, mo">St Louis</option>
-      <option value="joplin, mo">Joplin, MO</option>
-    </select>
-    <b>End:</b>
-    <select v-model="end">
-      <option value="chicago, il">Chicago</option>
-      <option value="st louis, mo">St Louis</option>
-      <option value="joplin, mo">Joplin, MO</option>
-    </select>
+        <span travelMode="DRIVING" :origin="origin" :destination="destionation"/>
+    </GoogleMap>
   </div>
-
-
-
-
-</div>
-
-
-
 </template>
 
 <script>
-
 import { ref } from 'vue'
 import API_ROUTER from './../../services/SERVER_API'
 import { GoogleMap, Marker } from 'vue3-google-map'
-import DirectionsRenderer from './../../components/Maps/MapsSilver.vue'
 
 export default {
     components: { 
-      GoogleMap, Marker, DirectionsRenderer
+      GoogleMap, Marker
     },
     data(){
 
       return{
         name: 'MapsCreate',
         Marcador: API_ROUTER.API_UI+'maps/location.png',
-        LaPlata: { lat: 2.3877584, lng: -75.8934048 },
+        LaPlata: { lat:  2.3867844, lng: -75.8944577 },
+        LaPlata2: { lat:  2.3886939, lng: -75.8949618 },
         Nataga: { lat: 2.545506, lng: -75.8096025 }, 
-        start: "",
-        end: "",
-        origin: { lat: 2.3877584, lng: -75.8934048 },
-        destination: { lat: 2.545506, lng: -75.8096025 },
-        travelMode: "driving"
+        travelMode: "DRIVING",
       }
     },
     mounted(){
 
     },
+    computed: {
+    origin() {
+
+      return { query: this.LaPlata };
+    },
+    destionation() {
+  
+      return { query: this.LaPlata2 };
+    }
+  },
 
     methods:{
 
-      hola: function(){        
+      hola: function(){    
 
-         origin = 'chicago, il'
-         destination = 'st louis, m'
-         travelMode = 'DRIVING'
+          
 
-        let directionsService = new window.google.maps.DirectionsService();
-        let directionsRenderer = new window.google.maps.DirectionsRenderer()
-
-        directionsService.route({  
-          origin: { lat: 2.3877584, lng: -75.8934048 },
-          destination: { lat: 2.545506, lng: -75.8096025 },  
-          travelMode: 'DRIVING' },
-          (response, status) => {
-            if (status !== "OK") return;
-            directionsRenderer.setDirections(response);
+       //google maps API's direction service
+      function calculateAndDisplayRoute(directionsService, directionsDisplay, start, destination) {
+        directionsService.route({
+          origin: start,
+          destination: destination,
+          travelMode: 'DRIVING'
+        }, function(response, status) {
+          if (status === 'OK') {
+            directionsDisplay.setDirections(response);
+            console.log(response)
+          } else {
+            window.alert('Directions request failed due to ' + status);
           }
-        );
+        });
+      }
+
+      var directionsService = new google.maps.DirectionsService;
+      var directionsDisplay = new google.maps.DirectionsRenderer;
+          directionsDisplay.setMap(this.$refs.map.$mapObject);
+
+      console.log(this.LaPlata);
+      console.log(this.Nataga);
+      
+      calculateAndDisplayRoute(directionsService, directionsDisplay, this.LaPlata, this.Nataga);
+      console.log('hmmm yha');
+
       },
   }
 }
