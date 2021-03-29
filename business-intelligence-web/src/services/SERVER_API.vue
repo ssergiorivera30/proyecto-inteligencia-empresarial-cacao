f6d078898fb244f6b1fca751a1309ed0982677b2
Yubeli30 <syrivera89@misena.edu.co>
2021-03-29 10:26:12 -0500

    Solución a error para la instalación de tailwindcss documentado

--- business-intelligence-web/src/services/SERVER_API.vue
+++ business-intelligence-web/src/services/SERVER_API.vue
@@ -2,15 +2,15 @@
 
     const API_ROUTER = {
 
-        API_UI : 'http://127.0.0.1/proyecto-inteligencia-empresarial-cacao/api/ui/',
+        API_UI : 'http://192.168.1.5/proyecto-inteligencia-empresarial-cacao/api/ui/',
 
-        API_CONTROLLER : 'http://127.0.0.1/proyecto-inteligencia-empresarial-cacao/api/controller/',
-        PHP7_CONTROLLER : 'http://127.0.0.1/proyecto-inteligencia-empresarial-cacao/api/php7/controller/',
+        API_CONTROLLER : 'http://192.168.1.5/proyecto-inteligencia-empresarial-cacao/api/controller/',
+        PHP7_CONTROLLER : 'http://192.168.1.5/proyecto-inteligencia-empresarial-cacao/api/php7/controller/',
         
-        API_FILE_SYSTEMS: 'http://127.0.0.1/proyecto-inteligencia-empresarial-cacao/api/file_systems/images/',
-        API_FACTURATE: 'http://127.0.0.1/proyecto-inteligencia-empresarial-cacao/api/file_systems/images/',
-        API_DOCUMENT: 'http://127.0.0.1/proyecto-inteligencia-empresarial-cacao/api/file_systems/images/',
-        LOGUIN: 'http://127.0.0.1:8080', 
+        API_FILE_SYSTEMS: 'http://192.168.1.5/proyecto-inteligencia-empresarial-cacao/api/file_systems/images/',
+        API_FACTURATE: 'http://192.168.1.5/proyecto-inteligencia-empresarial-cacao/api/file_systems/images/',
+        API_DOCUMENT: 'http://192.168.1.5/proyecto-inteligencia-empresarial-cacao/api/file_systems/images/',
+        LOGUIN: 'http://192.168.1.5:8080', 
     };
 
     export default API_ROUTER
