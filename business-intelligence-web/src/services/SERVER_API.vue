e4bd225bc06d154c6bff062fe2df393e48693ac1
Yubeli30 <syrivera89@misena.edu.co>
2021-03-30 14:42:27 -0500

    Update SERVER_API.vue

--- business-intelligence-web/src/services/SERVER_API.vue
+++ business-intelligence-web/src/services/SERVER_API.vue
@@ -2,15 +2,15 @@
 
     const API_ROUTER = {
 
-        API_UI : 'http://192.168.1.5/proyecto-inteligencia-empresarial-cacao/api/ui/',
+        API_UI : 'http://192.168.1.10/proyecto-inteligencia-empresarial-cacao/api/ui/',
 
-        API_CONTROLLER : 'http://192.168.1.5/proyecto-inteligencia-empresarial-cacao/api/controller/',
-        PHP7_CONTROLLER : 'http://192.168.1.5/proyecto-inteligencia-empresarial-cacao/api/php7/controller/',
+        API_CONTROLLER : 'http://192.168.1.10/proyecto-inteligencia-empresarial-cacao/api/controller/',
+        PHP7_CONTROLLER : 'http://192.168.1.10/proyecto-inteligencia-empresarial-cacao/api/php7/controller/',
         
-        API_FILE_SYSTEMS: 'http://192.168.1.5/proyecto-inteligencia-empresarial-cacao/api/file_systems/images/',
-        API_FACTURATE: 'http://192.168.1.5/proyecto-inteligencia-empresarial-cacao/api/file_systems/images/',
-        API_DOCUMENT: 'http://192.168.1.5/proyecto-inteligencia-empresarial-cacao/api/file_systems/images/',
-        LOGUIN: 'http://192.168.1.5:8080', 
+        API_FILE_SYSTEMS: 'http://192.168.1.10/proyecto-inteligencia-empresarial-cacao/api/file_systems/images/',
+        API_FACTURATE: 'http://192.168.1.10/proyecto-inteligencia-empresarial-cacao/api/file_systems/images/',
+        API_DOCUMENT: 'http://192.168.1.10/proyecto-inteligencia-empresarial-cacao/api/file_systems/images/',
+        LOGUIN: 'http://192.168.1.10:8080', 
     };
 
     export default API_ROUTER
