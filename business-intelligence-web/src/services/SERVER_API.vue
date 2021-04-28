f3cdbac06f5fa3d718b1ce5f574a22f3f4bdbce9
Sergio Rivera <syrivera89@misena.edu.co>
2021-04-28 09:04:39 -0500

    Update SERVER_API.vue

--- business-intelligence-web/src/services/SERVER_API.vue
+++ business-intelligence-web/src/services/SERVER_API.vue
@@ -2,24 +2,13 @@
 
     const API_ROUTER = {
 
-        API_UI : 'http://192.168.1.5/proyecto-inteligencia-empresarial-cacao/api/ui/',
-        API : 'http://192.168.1.5/proyecto-inteligencia-empresarial-cacao/api/',
-        API_CONTROLLER : 'http://192.168.1.5/proyecto-inteligencia-empresarial-cacao/api/controller/',
-        PHP7_CONTROLLER : 'http://192.168.1.5/proyecto-inteligencia-empresarial-cacao/api/php7/controller/',        
-        API_FILE_SYSTEMS: 'http://192.168.1.5/proyecto-inteligencia-empresarial-cacao/api/public/',     
-        LOGUIN: 'http://192.168.1.5:8080', 
+        API_UI : 'http://127.0.0.1/proyecto-inteligencia-empresarial-cacao/api/ui/',
+        API : 'http://127.0.0.1/proyecto-inteligencia-empresarial-cacao/api/',
+        API_CONTROLLER : 'http://127.0.0.1/proyecto-inteligencia-empresarial-cacao/api/controller/',
+        PHP7_CONTROLLER : 'http://127.0.0.1/proyecto-inteligencia-empresarial-cacao/api/php7/controller/',        
+        API_FILE_SYSTEMS: 'http://127.0.0.1/proyecto-inteligencia-empresarial-cacao/api/public/',     
+        LOGUIN: 'http://127.0.0.1:8080', 
     };
-
-    
-    // const API_ROUTER = {
-
-    //     API_UI : 'https://grupodeinvestigacionnova.com/cacao/software/api/ui/',
-    //     API : 'https://grupodeinvestigacionnova.com/cacao/software/api/',
-    //     API_CONTROLLER : 'https://grupodeinvestigacionnova.com/cacao/software/api/controller/',
-    //     PHP7_CONTROLLER : 'https://grupodeinvestigacionnova.com/cacao/software/api/php7/controller/',        
-    //     API_FILE_SYSTEMS: 'https://grupodeinvestigacionnova.com/cacao/software/api/public/',     
-    //     LOGUIN: 'https://grupodeinvestigacionnova.com/cacao/software/', 
-    // };
 
     export default API_ROUTER
 
