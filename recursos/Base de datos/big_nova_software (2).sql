d0ce91688c363d421fbf22970c1979488b6015bc
Sergio Rivera <syrivera89@misena.edu.co>
2021-04-30 17:58:00 -0500

    update

--- recursos/Base de datos/big_nova_software (2).sql
+++ recursos/Base de datos/big_nova_software (2).sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.0.4
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-04-2021 a las 08:41:50
--- Versión del servidor: 10.4.13-MariaDB
--- Versión de PHP: 7.4.8
+-- Tiempo de generación: 01-05-2021 a las 00:57:17
+-- Versión del servidor: 10.4.17-MariaDB
+-- Versión de PHP: 8.0.2
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -322,6 +322,32 @@ INSERT INTO `system_status` (`sys_auto_id`, `sys_status`, `sys_description`, `sy
 -- --------------------------------------------------------
 
 --
+-- Estructura de tabla para la tabla `tbl_services`
+--
+
+CREATE TABLE `tbl_services` (
+  `tbse_auto_id` int(11) NOT NULL,
+  `tbse_id_type_service` int(10) NOT NULL,
+  `tbse_name` varchar(120) COLLATE utf8_spanish_ci NOT NULL,
+  `tbse_description` text COLLATE utf8_spanish_ci NOT NULL,
+  `tbse_date_created` date NOT NULL,
+  `tbse_hour_created` date NOT NULL,
+  `tbse_updated` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `tbse_status` tinyint(1) NOT NULL DEFAULT 1,
+  `tbse_vigence` tinyint(1) NOT NULL DEFAULT 1
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `tbl_services`
+--
+
+INSERT INTO `tbl_services` (`tbse_auto_id`, `tbse_id_type_service`, `tbse_name`, `tbse_description`, `tbse_date_created`, `tbse_hour_created`, `tbse_updated`, `tbse_status`, `tbse_vigence`) VALUES
+(4, 1, 'Servicio Nacional de Aprendizaje', 'SENA', '2021-04-30', '2021-04-30', '2021-04-30 21:32:42', 1, 1),
+(5, 1, 'Servicio Nacional de Aprendizaje', 'Somos un establecimiento público del orden nacional, con personería jurídica, patrimonio propio e independiente, y autonomía administrativa;  Adscrito al Ministerio del Trabajo de Colombia. Ofrece formación gratuita a millones de colombianos que se benefician con programas técnicos, tecnológicos y complementarios que enfocados en el desarrollo económico, tecnológico y social del país, entran a engrosar las actividades productivas de las empresas y de la industria, para obtener mejor competitividad y producción con los mercados globalizados. \r\n\r\nFacultada por el Estado para la inversión en infraestructura necesaria para mejorar el desarrollo social y técnico de los trabajadores en las diferentes regiones,  a través de formación profesional integral que logra incorporarse con las metas del Gobierno Nacional, mediante el cubrimiento de las necesidades específicas de recurso humano en las empresas, a través de la vinculación al mercado laboral -bien sea como empleado o subempleado-, con grandes oportunidades para el desarrollo empresarial, comunitario y tecnológico.  \r\n\r\nLa entidad más querida por los colombianos funciona en permanente alianza entre Gobierno, empresarios y trabajadores, desde su creación, con el firme propósito de lograr la competitividad de Colombia a través del incremento de la productividad en las empresas y regiones, sin dejar de lado la inclusión social, en articulación con la política nacional: Más empleo y menos pobreza. Por tal razón, se generan continuamente programas y proyectos de responsabilidad social, empresarial, formación, innovación, internacionalización y transferencia de conocimientos y tecnologías.', '2021-04-30', '2021-04-30', '2021-04-30 22:38:53', 1, 1);
+
+-- --------------------------------------------------------
+
+--
 -- Estructura de tabla para la tabla `users_avatars`
 --
 
@@ -568,6 +594,12 @@ ALTER TABLE `system_status`
   ADD PRIMARY KEY (`sys_auto_id`);
 
 --
+-- Indices de la tabla `tbl_services`
+--
+ALTER TABLE `tbl_services`
+  ADD PRIMARY KEY (`tbse_auto_id`);
+
+--
 -- Indices de la tabla `users_avatars`
 --
 ALTER TABLE `users_avatars`
@@ -691,6 +723,12 @@ ALTER TABLE `projects_users`
 --
 ALTER TABLE `system_status`
   MODIFY `sys_auto_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- AUTO_INCREMENT de la tabla `tbl_services`
+--
+ALTER TABLE `tbl_services`
+  MODIFY `tbse_auto_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT de la tabla `users_keys`
