c6477e4ecc8c7961cc8c0fa184f45623d3789503
Sergio Rivera <syrivera89@misena.edu.co>
2021-05-19 10:28:10 -0500

    Update SERVER_API.vue

--- business-intelligence-web/src/services/SERVER_API.vue
+++ business-intelligence-web/src/services/SERVER_API.vue
@@ -2,11 +2,11 @@
 
     const API_ROUTER = {
 
-        API_UI : 'http://192.168.1.4/proyecto-inteligencia-empresarial-cacao/api/ui/',
-        API : 'http://192.168.1.4/proyecto-inteligencia-empresarial-cacao/api/',
-        API_CONTROLLER : 'http://192.168.1.4/proyecto-inteligencia-empresarial-cacao/api/controller/',
-        PHP7_CONTROLLER : 'http://192.168.1.4/proyecto-inteligencia-empresarial-cacao/api/php7/controller/',        
-        API_PUBLIC: 'http://192.168.1.4/proyecto-inteligencia-empresarial-cacao/api/public/',     
+        API_UI : 'http://127.0.0.1/proyecto-inteligencia-empresarial-cacao/api/ui/',
+        API : 'http://127.0.0.1/proyecto-inteligencia-empresarial-cacao/api/',
+        API_CONTROLLER : 'http://127.0.0.1/proyecto-inteligencia-empresarial-cacao/api/controller/',
+        PHP7_CONTROLLER : 'http://127.0.0.1/proyecto-inteligencia-empresarial-cacao/api/php7/controller/',        
+        API_PUBLIC: 'http://127.0.0.1/proyecto-inteligencia-empresarial-cacao/api/public/',     
         LOGUIN: 'http://192.164.1.3:8080', 
     };
 
