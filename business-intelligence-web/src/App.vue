b2c8fe532b333a83a7163da4357d7561c97ca5f0
Sergio Rivera <syrivera89@misena.edu.co>
2021-06-02 17:56:07 -0500

    Update ui

--- business-intelligence-web/src/App.vue
+++ business-intelligence-web/src/App.vue
@@ -17,44 +17,8 @@
     
     <div class="bg-gray-50 min-h-screen flex-grow text-gray-800 ">
 
-    <aside class="print-none hidden sm:flex sm:flex-col fixed min-h-screen">
-      
-      <div class="flex-grow flex flex-col justify-between text-gray-500 w-14 mt-14">
-        <nav class="flex flex-col mx-4 my-2 space-y-3">
-          <router-link to="/inicio"
-            class="inline-flex items-center justify-center py-2 hover:text-principal-color-ui focus:text-principal-color-ui rounded-lg">
-            <span class="sr-only">Inicio</span>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-              class="h-6 w-6">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" />
-            </svg>
-          </router-link>
-          <router-link to="/herraminetas"
-            class="inline-flex items-center justify-center py-2 hover:text-principal-color-ui focus:text-principal-color-ui rounded-lg"
-            title="Herramientas">
-            <span class="sr-only">Herramientas</span>
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor"
-              class="h-6 w-6">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M17 14v6m-3-3h6M6 10h2a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2zm10 0h2a2 2 0 002-2V6a2 2 0 00-2-2h-2a2 2 0 00-2 2v2a2 2 0 002 2zM6 20h2a2 2 0 002-2v-2a2 2 0 00-2-2H6a2 2 0 00-2 2v2a2 2 0 002 2z" />
-            </svg>
-          </router-link>
+      <NavAside />
 
-
-          <router-link to="/cuenta/ver"
-            class="inline-flex items-center justify-center py-2 hover:text-principal-color-ui focus:text-principal-color-ui rounded-lg">
-            <span class="sr-only">Settings</span>
-            <svg aria-hidden="true" fill="none" viewBox="0 0 24 24" stroke="currentColor" class="h-6 w-6">
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M10.325 4.317c.426-1.756 2.924-1.756 3.35 0a1.724 1.724 0 002.573 1.066c1.543-.94 3.31.826 2.37 2.37a1.724 1.724 0 001.065 2.572c1.756.426 1.756 2.924 0 3.35a1.724 1.724 0 00-1.066 2.573c.94 1.543-.826 3.31-2.37 2.37a1.724 1.724 0 00-2.572 1.065c-.426 1.756-2.924 1.756-3.35 0a1.724 1.724 0 00-2.573-1.066c-1.543.94-3.31-.826-2.37-2.37a1.724 1.724 0 00-1.065-2.572c-1.756-.426-1.756-2.924 0-3.35a1.724 1.724 0 001.066-2.573c-.94-1.543.826-3.31 2.37-2.37.996.608 2.296.07 2.572-1.065z" />
-              <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
-            </svg>
-          </router-link>
-        </nav>
-      </div>
-    </aside>
 
 
       <!-- bg-gradient-to-r from-blue-700 via-indigo-600 to-purple-600 -->
@@ -129,14 +93,16 @@
   import 'firebase/auth';
   import axios from 'axios'
   import API_ROUTER from './services/SERVER_API'
-  import NavNotificaciones from './components/NavBar/NavNotificaciones.vue'
+  import NavNotificaciones from './components/HeaderApp/NavNotificaciones.vue'
+  import NavAside from './components/HeaderApp/NavAside.vue'
 
   const ContainerCenter = "lg:m-auto"
 
 
   export default {
     components: {
-      NavNotificaciones
+      NavNotificaciones,
+      NavAside
     },
     data() {
       return {
--- business-intelligence-web/src/views/BuildService/BuildFirsStep.vue
+++ business-intelligence-web/src/views/BuildService/BuildFirsStep.vue
@@ -1,84 +1,102 @@
 <template>
   <div class="grid grid-cols-1 md:grid-cols-2 gap-4 mt-2">
-    <form
-      action="#"
-      method="POST"
-      class="space-y-2 mt-5"
-      @submit.prevent="SaveInitialForm()">
-      <header class="flex items-center justify-center">
-        <h2 v-if="ServiceType == 1" class="text-lg leading-6 font-medium text-black">Creación de <span class="font-bold border-b border-black">grupo</span></h2>
-        <h2 v-if="ServiceType == 2" class="text-lg leading-6 font-medium text-black">Creación de <span class="font-bold border-b border-black">proyecto</span></h2>
-        <h2 v-if="ServiceType == 3" class="text-lg leading-6 font-medium text-black">Creación de <span class="font-bold border-b border-black">entidad</span></h2>
-        <h2 v-if="ServiceType == 4" class="text-lg leading-6 font-medium text-black">Creación de <span class="font-bold border-b border-black">formulario</span></h2>
+    <form action="#" method="POST" class="space-y-2 mt-5" @submit.prevent="SaveInitialForm()">
+
+      <header class="flex items-center">
+        <h2 class="text-lg leading-6 font-medium text-black">Creación de
+          <span class="font-bold">{{ NameTypeService }}</span>
+        </h2>
       </header>
 
       <div>
         <label class="text-gray-700 font-semibold text-xs">Nombre</label>
-        <input
-          type="text"
-          class="form-control2"
-          required
-          v-model="ServiceName"
-        />
+        <input type="text" class="form-control2" required v-model="ServiceName" />
       </div>
 
       <div v-if="ServiceType == 2">
         <label class="text-gray-700 font-semibold text-xs">Nombre de la Empresa, entidad o programa de formación</label>
-        <input
-          type="text"
-          class="form-control2"
-          required
-          v-model="ServiceNameEntity"
-        />
+        <input type="text" class="form-control2" required v-model="ServiceNameEntity" />
       </div>
 
       <div v-if="ServiceType == 1">
         <label class="text-gray-700 font-semibold text-xs">Logo</label>
-        <input
-          type="file"
-          class="form-control2"
-          required
-        />
+        <input type="file" class="form-control2" required />
       </div>
 
       <div>
         <label class="text-gray-700 font-semibold text-xs">Breve descripción</label>
-        <textarea
-          wrap="hard"
-          rows="7"
-          class="form-control2"
-          required
-          v-model="ServiceDescription"
-        ></textarea>
+        <textarea wrap="hard" rows="7" class="form-control2" required v-model="ServiceDescription"></textarea>
       </div>
 
-
-      <div v-if="ServiceType == 1 || ServiceType == 2 || ServiceType == 4"> 
+      <div v-if="ServiceType == 1 || ServiceType == 2 || ServiceType == 4">
         <label class="text-gray-700 font-semibold text-xs">Integrantes</label>
-        <input
-          type="text"
-          class="form-control2"
-          required
-        />
+        <input type="text" class="form-control2" placeholder="Buscar usuario" />
       </div>
+
+
+      <div class="py-4 flex items-center flex-no-wrap">
+        <div class="w-12 h-12 bg-cover bg-center rounded-md">
+          <img src="https://tuk-cdn.s3.amazonaws.com/assets/components/avatars/a_4_0.png" alt=""
+            class="h-full w-full overflow-hidden object-cover rounded-full border-2 border-white dark:border-gray-700 shadow" />
+        </div>
+        <div class="w-12 h-12 bg-cover rounded-md -ml-2">
+          <img src="https://tuk-cdn.s3.amazonaws.com/assets/components/avatars/a_4_1.png" alt=""
+            class="h-full w-full overflow-hidden object-cover rounded-full border-2 border-white dark:border-gray-700 shadow" />
+        </div>
+        <div class="w-12 h-12 bg-cover rounded-md bg-center -ml-2">
+          <img src="https://tuk-cdn.s3.amazonaws.com/assets/components/avatars/a_4_2.png" alt=""
+            class="h-full w-full overflow-hidden object-cover rounded-full border-2 border-white dark:border-gray-700 shadow" />
+        </div>
+        <div class="w-12 h-12 bg-cover rounded-md -ml-2">
+          <img src="https://tuk-cdn.s3.amazonaws.com/assets/components/avatars/a_4_3.png" alt=""
+            class="h-full w-full overflow-hidden object-cover object-center rounded-full border-2 border-white dark:border-gray-700 shadow" />
+        </div>
+        <div class="w-12 h-12 bg-cover rounded-md -ml-2">
+          <img src="https://tuk-cdn.s3.amazonaws.com/assets/components/avatars/a_4_4.png" alt=""
+            class="h-full w-full overflow-hidden object-cover object-center rounded-full border-2 border-white dark:border-gray-700 shadow" />
+        </div>
+        <div class="w-12 h-12 bg-cover rounded-md -ml-2">
+          <img src="https://tuk-cdn.s3.amazonaws.com/assets/components/avatars/a_4_4.png" alt=""
+            class="h-full w-full overflow-hidden object-cover object-center rounded-full border-2 border-white dark:border-gray-700 shadow" />
+        </div>
+        <div class="w-12 h-12 bg-cover rounded-md -ml-2">
+          <img src="https://tuk-cdn.s3.amazonaws.com/assets/components/avatars/a_4_4.png" alt=""
+            class="h-full w-full overflow-hidden object-cover object-center rounded-full border-2 border-white dark:border-gray-700 shadow" />
+        </div>
+        <div class="w-12 h-12 bg-cover rounded-md -ml-2">
+          <img src="https://tuk-cdn.s3.amazonaws.com/assets/components/avatars/a_4_4.png" alt=""
+            class="h-full w-full overflow-hidden object-cover object-center rounded-full border-2 border-white dark:border-gray-700 shadow" />
+        </div>
+        <div class="w-12 h-12 bg-cover rounded-md -ml-2">
+          <img src="https://tuk-cdn.s3.amazonaws.com/assets/components/avatars/a_4_4.png" alt=""
+            class="h-full w-full overflow-hidden object-cover object-center rounded-full border-2 border-white dark:border-gray-700 shadow" />
+        </div>
+        <div class="w-12 h-12 bg-cover rounded-md -ml-2">
+          <img src="https://tuk-cdn.s3.amazonaws.com/assets/components/avatars/a_4_4.png" alt=""
+            class="h-full w-full overflow-hidden object-cover object-center rounded-full border-2 border-white dark:border-gray-700 shadow" />
+        </div>
+ 
+        <div class="w-12 h-12 bg-cover rounded-md bg-center -ml-2">
+          <img src="https://tuk-cdn.s3.amazonaws.com/assets/components/avatars/a_4_2.png" alt=""
+            class="h-full w-full overflow-hidden object-cover rounded-full border-2 border-white dark:border-gray-700 shadow" />
+        </div>
+        <div class="w-12 h-12 bg-cover rounded-md -ml-2">
+          <img src="https://tuk-cdn.s3.amazonaws.com/assets/components/avatars/a_4_3.png" alt=""
+            class="h-full w-full overflow-hidden object-cover object-center rounded-full border-2 border-white dark:border-gray-700 shadow" />
+        </div>
+      </div>
+
 
       <div class="py-2 text-left">
-        <button
-          type="submit"
-          class="inline-flex justify-center py-1 px-2 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-my-primary focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        >
+        <button type="submit" class="btn-primary">
           Continuar
         </button>
       </div>
     </form>
 
-    <PreviewForm
-      v-if="ServiceName != ''"
-      :NameForm="ServiceName"
-      :DescriptionForm="ServiceDescription"
-    />
+    <PreviewForm v-if="ServiceName != ''" :NameForm="ServiceName" :DescriptionForm="ServiceDescription" />
 
-    <div v-else class="flex justify-center">
+    <div v-else class="hidden md:flex justify-center">
       <img v-if="this.$route.params.type_service == 1" class="w-2/3 p-10" :src="service_group" alt="" />
       <img v-if="this.$route.params.type_service == 2" class="w-2/3 p-10" :src="service_project" alt="" />
       <img v-if="this.$route.params.type_service == 3" class="w-2/3 p-10" :src="service_entity" alt="" />
@@ -87,57 +105,80 @@
   </div>
 </template>
 <script>
-import axios from "axios";
-import Noty from "noty";
-import API_ROUTER from "./../../services/SERVER_API";
-import PreviewForm from "./../../components/Formularios/PreviewForm";
+  import axios from "axios";
+  import Noty from "noty";
+  import API_ROUTER from "./../../services/SERVER_API";
+  import PreviewForm from "./../../components/Formularios/PreviewForm";
 
-export default {
-  name: "BuildFirsStep",
-  components: {
-    PreviewForm,
-  },
-  data() {
-    return {
-      service_group: API_ROUTER.API_UI + "services/team.svg",
-      service_project: API_ROUTER.API_UI + "services/projects.svg",
-      service_form: API_ROUTER.API_UI + "services/forms.svg",
-      service_entity: API_ROUTER.API_UI + "services/entity.svg",
-
-      ServiceType: this.$route.params.type_service,
-      ServiceName: "",
-      ServiceNameEntity: null,
-      ServiceDescription: "",
-    };
-  },
-  mounted: function () {},
-  methods: {
-    SaveInitialForm: function () {
-      axios
-        .post(API_ROUTER.PHP7_CONTROLLER + "build/create_build_first_step.php", {
-          ServiceType: this.$route.params.type_service,
-          ServiceName: this.ServiceName,
-          ServiceDescription: this.ServiceDescription,
-        })
-        .then((response) => {
-
-          new Noty({
-            theme: "sunset",
-            layout: "topRight",
-            progressBar: true,
-            closeWith: ["click", "button"],
-            timeout: 8000,
-            type: response.data.icono,
-            text: response.data.mensaje,
-          }).show();
-
-          if(response.data.icono == 'success'){
-            this.$router.replace("/constructor-service/" + response.data.code);
-          }
-
-          
-        });
+  export default {
+    name: "BuildFirsStep",
+    components: {
+      PreviewForm,
     },
-  },
-};
+    data() {
+      return {
+        service_group: API_ROUTER.API_UI + "services/team_2.svg",
+        service_project: API_ROUTER.API_UI + "services/project_2.svg",
+        service_entity: API_ROUTER.API_UI + "services/entity_2.svg",
+        service_form: API_ROUTER.API_UI + "services/form_2.svg",
+
+        NameTypeService: '',
+
+        ServiceType: this.$route.params.type_service,        
+        ServiceName: "",
+        ServiceNameEntity: null,
+        ServiceDescription: "",
+      };
+    },
+    mounted: function () {
+
+      if (this.$route.params.type_service == 1) {
+        this.NameTypeService = 'grupo'
+      }
+      else if (this.$route.params.type_service == 2) {
+        this.NameTypeService = 'proyecto'
+      }
+      else if (this.$route.params.type_service == 3) {
+        this.NameTypeService = 'entidad'
+      }
+
+      else if (this.$route.params.type_service == 4) {
+        this.NameTypeService = 'formulario'
+      }
+
+      else {
+
+      }
+
+
+    },
+    methods: {
+      SaveInitialForm: function () {
+        axios
+          .post(
+            API_ROUTER.PHP7_CONTROLLER + "build/create_build_first_step.php",
+            {
+              ServiceType: this.$route.params.type_service,
+              ServiceName: this.ServiceName,
+              ServiceDescription: this.ServiceDescription,
+            }
+          )
+          .then((response) => {
+            new Noty({
+              theme: "sunset",
+              layout: "topRight",
+              progressBar: true,
+              closeWith: ["click", "button"],
+              timeout: 8000,
+              type: response.data.icono,
+              text: response.data.mensaje,
+            }).show();
+
+            if (response.data.icono == "success") {
+              this.$router.replace("/constructor-service/" + response.data.code);
+            }
+          });
+      },
+    },
+  };
 </script>
